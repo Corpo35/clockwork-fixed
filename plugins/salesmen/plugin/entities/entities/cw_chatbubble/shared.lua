1e3d104bb1d669e19920875029c9ce1ea8bbb6b6
thinpress <87988308+Corpo35@users.noreply.github.com>
2025-07-25 00:27:57 +0300

    Re-organize file structure.

--- plugins/salesmen/plugin/entities/entities/cw_chatbubble/shared.lua
+++
@@ -1,7 +0,0 @@
-
-ENT.Type = "anim"
-ENT.Base = "base_anim"
-ENT.Author = "kurozael"
-ENT.PrintName = "Chat Bubble"
-ENT.Spawnable = false
-ENT.AdminSpawnable = false
--- plugins/salesmen/plugin/sh_plugin.lua
+++
@@ -1,8 +0,0 @@
-
-PLUGIN:SetGlobalAlias("cwSalesmen")
-
-Clockwork.kernel:IncludePrefixed("cl_plugin.lua")
-Clockwork.kernel:IncludePrefixed("sv_plugin.lua")
-Clockwork.kernel:IncludePrefixed("sv_hooks.lua")
-
-cwSalesmen.salesmen = {}
